835256d5a3bce4a261ee49f52b5c0d6c5f95ac33
AustinSchlutter <90343739+austinjames95@users.noreply.github.com>
2025-06-20 12:28:15 -0700

    Create config.py

---
+++ 6.20.2025/config.py
@@ -0,0 +1,6 @@
+"""
+Inscert Directories in Given Locations
+"""
+base_dir = r'C:\Users\austi\OneDrive\Desktop\COH\patientData'
+given_pvh_dir = r'C:\Users\austi\OneDrive\Desktop\COH\pvh_t1.csv'
+given_dvh_dir = None
